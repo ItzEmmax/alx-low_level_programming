5c3aae5d67e25e13ef6c0d9b01d9471a950ce28c
Onyeka Emmanuel <onyekaemmanuel995@gmail.com>
2023-06-20 23:06:36 -0700

    task 4 added

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks if characters are in both upper or lower case
+ *
+ * @c: takes it input from other functions.
+ *
+ * Return: return 1 if is true, if not return 0
+ *
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 97 && c < 122)
+		return (1);
+	if (c >= 65 && c <= 90)
+		return (1);
+
+	return (0);
+}
