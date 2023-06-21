aa1d23cf43c8e74a710ed119da3fd8c784bad5d5
Onyeka Emmanuel <onyekaemmanuel995@gmail.com>
2023-06-20 22:23:29 -0700

    task 1 added

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,10 +9,10 @@
 
 void print_alphabet(void)
 {
-	int letter;
+	int ch;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
-		_putchar(letter);
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
 
-	_putchar("\n");
+	_putchar('\n');
 }
