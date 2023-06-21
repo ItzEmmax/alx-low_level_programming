7900e60335cb02abdcc5877695142e1c5ca52f8b
Onyeka Emmanuel <onyekaemmanuel995@gmail.com>
2023-06-21 00:27:09 -0700

    task 10 added

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - Prints the addition of two integers
+ *
+ * @a: input 1
+ * @b: input 2
+ *
+ * Return: Sum of a and b
+ *
+ */
+
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
