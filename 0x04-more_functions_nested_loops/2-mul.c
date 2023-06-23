6628193f995af324ea6dab8a6339722d7de6b1a2
Onyeka Emmanuel <onyekaemmanuel995@gmail.com>
2023-06-22 18:03:31 -0700

    Adding task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - function that multipliers two integers
+ *
+ * @a: first input
+ * @b: second input
+ *
+ * Return: Always 0
+ *
+ */
+
+int mul(int a, int b);
+{
+	return (a * b);
+}
