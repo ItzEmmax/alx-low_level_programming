3c9faf3d21b623eb2a348ebc8a3d20caf83e2b8d
Onyeka Emmanuel <onyekaemmanuel995@gmail.com>
2023-06-23 23:04:38 -0700

    Adding task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_line - Prints a straight line with _ char
+ *
+ * @n: is the number of times the _ char will be printed
+ *
+ */
+
+void print_line(int n)
+{
+	int lnChr;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (lnChr = 1; lnChr <= n; lnChr++)
+			_putchar('_');
+	}
+}
