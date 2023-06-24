3ee8a4f4b36c80643e000e80fa6645d127a87695
Onyeka Emmanuel <onyekaemmanuel995@gmail.com>
2023-06-24 04:33:59 -0700

    Adding task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,13 +10,13 @@
 
 void print_most_numbers(void)
 {
-	int num;
+	char c;
 
-	for (num = 0; num <= 9; num++)
+	for (c = '0'; c <= '9'; c++)
 	{
-		if (num == 2 || num == 4)
+		if (!(c == '2' || c == '4'))
 			continue;
-		_putchar(num + 48);
+		_putchar(c);
 	}
 	_putchar('\n');
 }
