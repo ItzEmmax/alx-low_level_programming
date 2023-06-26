30d42257694943c11cc1a12d136f9d0664865f3f
Onyeka Emmanuel <onyekaemmanuel995@gmail.com>
2023-06-26 16:24:53 -0700

    First commit, Adding task 0 and README.md file

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Function pointer
+ *
+ * Desc: function to point and update value of n to 98
+ *
+ * @n: input
+ *
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
