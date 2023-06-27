c8bc2df8b2b3289c0cbf83be23df396f4c0aef49
Onyeka Emmanuel <onyekaemmanuel995@gmail.com>
2023-06-26 17:15:45 -0700

    Adding task 2

---
+++ 0x05-pointers_arrays_strings/ !betty 2-strlen.c
@@ -0,0 +1,12 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strlen - Returns length of a string
+ *
+ */
+
+int _strlen(char* s)
+{
+	intlen = strlen(s);
+}
---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strlen - Returns length of a string
+ *
+ * @s: string
+ *
+ * Return: len
+ *
+ */
+
+int _strlen(char *s)
+{
+	intlen = strlen(s);
+	return (len);
+}
