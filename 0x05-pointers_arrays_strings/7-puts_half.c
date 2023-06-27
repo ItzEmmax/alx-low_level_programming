7852c7e8c7f903a03b56f46ad6ad48919fb96545
Onyeka Emmanuel <onyekaemmanuel995@gmail.com>
2023-06-27 14:35:10 -0700

    Adding task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts_half -  Prints half of a string followed by a new line
+ *
+ * @str: string input
+ *
+ */
+
+void puts_half(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+
+	i++;
+
+	for (i /= 2; str[i] != '\0'; i++)
+	{
+		putchar(str[i]);
+	}
+	putcharx('\n');
+}
