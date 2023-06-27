c82574ce2d2e72623687179d70423ff1bd4130de
Onyeka Emmanuel <onyekaemmanuel995@gmail.com>
2023-06-26 17:58:19 -0700

    Adding task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints string in reverse
+ *
+ * @s: string
+ */
+
+void print_rev(char *s)
+{
+	int length = strlen(s);
+
+	for (int i = length - 1; i >= 0; 1--)
+	{
+		printf("%c", s[i]);
+	}
+	printf("\n")';
+}
