206dc46c6f0e9453aea452ee0faf376f72ec2e4a
Onyeka Emmanuel <onyekaemmanuel995@gmail.com>
2023-06-29 17:32:28 -0700

    Adding opt task 101

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,16 +8,17 @@ void print_number(int n)
 {
 	unsigned int n1;
 
-	n1 = n;
-
 	if (n < 0)
 	{
-		putchar('-');
 		n1 = -n;
+		_putchar('-');
+	} else
+	{
+		n1 = n;
 	}
 
-	if (n1 / 10 != 0)
+	if (n1 / 10)
 		print_number(n1 / 10);
 
-	putchar((n1 % 10) + '0');
+	_putchar((n1 % 10) + '0');
 }
