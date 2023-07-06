70ea9753cb13f1524775e3e8850e5f4049eff80d
Onyeka Emmanuel <onyekaemmanuel995@gmail.com>
2023-07-05 19:52:38 -0700

    Adding task 1 and 1

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,21 +2,15 @@
 
 /**
  * _puts_recursion - prints a string, followed by a new line
- *
  * @s: string to print
- *
- * Return: void
  */
-
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
 		_putchar('\n');
+		return;
 	}
-	else
-	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
-	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
